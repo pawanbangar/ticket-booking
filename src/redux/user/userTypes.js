8e7055bb95ec9dfa2eb1e579ae91045798c22482
pawanbangar <>
2023-02-02 02:20:57 +0530

    removed user reducer

--- src/redux/user/userTypes.js
+++
@@ -1,9 +0,0 @@
-const userActionTypes = {
-  FETCH_USERS: "FETCH_USERS",
-  CREATE_USER: "CREATE_USER",
-  UPDATE_USER: "UPDATE_USER",
-  DELETE_USER: "DELETE_USER",
-  SET_PER_PAGE: "SET_PER_PAGE",
-};
-
-export default userActionTypes;
--- src/redux/user/userUtil.js
+++
@@ -1,8 +0,0 @@
-function getUserFromId(users, id) {
-  // filter by id and get first record
-  return users.find((element) => {
-    return element.id === id;
-  });
-}
-
-export default getUserFromId;
